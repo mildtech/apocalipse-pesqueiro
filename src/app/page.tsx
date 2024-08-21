f0d990c66ef825b7e51d331e2b1b5108766b0004
rodrigojorge <rodrigojorge@gmail.com>
2024-08-21 20:07:30 -0300

    inclusao das quantidades pescadas por jogador

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
  "use client"
 
-import { use, useEffect, useRef, useState } from 'react';
+import { use, useCallback, useEffect, useRef, useState } from 'react';
 import Jogador from './components/Jogador';
 import Tabela from './components/Tabela';
-import { insertCoin, onPlayerJoin, useMultiplayerState, usePlayersList } from 'playroomkit';
+import { insertCoin, myPlayer, onPlayerJoin, useMultiplayerState, usePlayersList } from 'playroomkit';
+import { get } from 'http';
 
 type Rodada = {
   numero: number;
@@ -19,6 +20,7 @@ type Jogador = {
 
 type GameState = {
   limiteSustentavel: number; 
+  limitePossivelRodada: number;
   taxaCrescimento: number;
   custoFiscalizacao: number;
   quantidadeInicialPeixesJogador: number;
@@ -28,6 +30,7 @@ type GameState = {
 
 const initialState: GameState = {
   limiteSustentavel: 11,
+  limitePossivelRodada: 20,
   taxaCrescimento: 1.02,
   custoFiscalizacao: 2,
   quantidadeInicialPeixesJogador: 100,
@@ -35,14 +38,18 @@ const initialState: GameState = {
   rodadas: []
 }
 
+const PEIXES_CESTO = 'peixesCesto';
+
 
 export default function Home() {
   
-  const quantidadePescada = useRef<HTMLInputElement>(null);
+  const quantidadePescadaRef = useRef<HTMLInputElement>(null);
 
   const [gameState, setGameState] = useMultiplayerState('gameState', initialState);
+  const [peixesCesto, setPeixesCesto] = useState<number>(0);
+  const [error, setError] = useState<string | null>(null);
 
-  const jogadores = usePlayersList();
+  const jogadores = usePlayersList(true);
 
   useEffect(() => {
     async function setGame() {
@@ -52,13 +59,13 @@ export default function Home() {
           // PlayerState is this player's multiplayer state along with it's profile.
           // Probably add a player sprite to the game here.
           console.log(playerState.getProfile().name + ' joined the game'); 
-          playerState.setState("totalPeixesPescados", 0);
-          console.log('novaQuantgameState.quantidadePeixesLago: ', gameState.quantidadePeixesLago);
+          playerState.setState(PEIXES_CESTO, 0);
+          /*console.log('novaQuantgameState.quantidadePeixesLago: ', gameState.quantidadePeixesLago);
           const novaQuantidadePeixes = gameState.quantidadePeixesLago + gameState.quantidadeInicialPeixesJogador;
           console.log('novaQuantidadePeixes: ', novaQuantidadePeixes);
           
           gameState.quantidadePeixesLago = novaQuantidadePeixes;
-          setGameState(gameState, true);
+          setGameState(gameState, true);*/
 
           playerState.onQuit(() => {
             // Handle player quitting. Maybe remove player sprite?
@@ -76,8 +83,29 @@ export default function Home() {
   }
 
   function handlePescar() {
-    console.log('Pescar - quantidadePescada: ', quantidadePescada.current?.value);
+    const quantidadePescada = Number(quantidadePescadaRef.current?.value);
+    console.log('Pescar - quantidadePescada: ', quantidadePescada);
+    if (quantidadePescada > gameState.limitePossivelRodada) {
+      setError('Quantidade de peixes pescados maior que o limite possível por rodada');
+      return;
+    }
+    const totalPescadoRodada = peixesCesto + quantidadePescada;
+    setPeixesCesto(totalPescadoRodada);
+    myPlayer()?.setState(PEIXES_CESTO, totalPescadoRodada, true);
   }
+
+  const getTotalPeixesLago = useCallback(() => {
+    let totalPeixesPescados = 0;
+    
+    jogadores.forEach(jogador => {
+      const peixesCesto = jogador.getState(PEIXES_CESTO);
+      totalPeixesPescados += peixesCesto;
+    });
+    const totalPeixesLago = (jogadores.length * gameState.quantidadeInicialPeixesJogador) - totalPeixesPescados;
+    gameState.quantidadePeixesLago = totalPeixesLago;
+    
+    return totalPeixesLago;
+  },[gameState, jogadores]);
 
   return (  
     <main>
@@ -85,9 +113,10 @@ export default function Home() {
         <div id="cabecalho">
           Apolicapse Pesqueiro: 
         </div>
+        <Jogador key={myPlayer()?.id} nome={myPlayer()?.getProfile().name} quantidadeTotalPescada={peixesCesto} onClick={handleJogadorClick}/>
         <div id="configJogo">
           <div>
-            Total de Peixes no lago: {gameState.quantidadePeixesLago}
+            Total de Peixes no lago: {getTotalPeixesLago()}
           </div>
           <div>
             Limite Sustentável: {gameState.limiteSustentavel}
@@ -97,14 +126,15 @@ export default function Home() {
           </div>
         </div>
         <label htmlFor="quantidadePescada">Quantidade de peixes pescados: </label>
-        <input type="number" ref={quantidadePescada} id="quantidadePescada" name="quantidadePescada" min="0" max="100" />
+        <input type="number" ref={quantidadePescadaRef} id="quantidadePescada" name="quantidadePescada" min="0" max="100" />
         
         <div id="demaisJogadores">
           {jogadores.map(jogador => {
             return <Jogador key={jogador.id} nome={jogador.getProfile().name} onClick={handleJogadorClick}/>
           })}   
         </div>
-
+        {error ? <div className='absolute inset-0 bg-red-500' onClick={()=>setError(null)}>{error}</div> : null}
+        
         <button onClick={handlePescar} > Pescar </button>
     </main>
   );
